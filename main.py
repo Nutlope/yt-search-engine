3fb52080b1ca5569b3184c57d7d926e153463529
nutlope <he55@drexel.edu>
2021-04-13 13:47:07 -0400

    refactored main file

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import subprocess
+
 url = "https://www.youtube.com/watch?v=49_FlYOSxgE&ab_channel=BenLionelScott"
-process = subprocess.run(["youtube-dl", "--extract-audio", "--audio-format", "wav", "-o something.wav", url], universal_newlines=True)
-# output is in process.stdout
+process = subprocess.run(["youtube-dl", "--extract-audio", "--audio-format", "wav", "-o something.wav", url], 
+                        universal_newlines=True) # output is in process.stdout
 
-# Old way of doing it
-# import os
-
-# url = "https://www.youtube.com/watch?v=49_FlYOSxgE&ab_channel=BenLionelScott"
-# os.system('youtube-dl --extract-audio --audio-format wav -o something.wav {}'.format(url))

2b8e0b1ac305928d603901ea2248b3be299c95c5
nutlope <he55@drexel.edu>
2021-04-13 16:37:32 -0400

    added support for wit.ai speech recognition

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -1,10 +1,21 @@
 import speech_recognition as sr
 
-# Transcribing audio file and outputting text to console
+# Configuration
+fileName = "audio_transcripts/harvard.wav" # name of file to be transcribed
+recognitionType = 'google' # type of speech recognition (google, wit.ai)
+
+# Transcribing audio file and outputting text to console 
 r = sr.Recognizer()
-motivational = sr.AudioFile("audio_transcripts/yes.wav")
+motivational = sr.AudioFile(fileName)
 with motivational as source:
     audio = r.record(source)
 
-text = r.recognize_google(audio)
+if recognitionType == 'google':
+    print("The google translation:") # Using google speech recognition
+    text = r.recognize_google(audio)
+elif recognitionType == 'wit.ai':
+    print("The wit.ai translation:") # Using wit.ai speech recognition
+    WIT_AI_KEY = 'NTMKQ4AE7B633R4WRGJCLBZPB5XFFDTC'
+    text = r.recognize_wit(audio, key=WIT_AI_KEY)
 print(text)
+

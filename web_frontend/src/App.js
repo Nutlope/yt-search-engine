bbd808a68831267526739d2052c2cdce488f2765
nutlope <he55@drexel.edu>
2021-04-20 12:48:45 -0400

    fixed Chakra form bug, added duck logo

--- web_frontend/src/App.js
+++ web_frontend/src/App.js
@@ -2,21 +2,24 @@ import "./App.css";
 import HomePage from "./Components/HomePage.js";
 import SearchResults from "./Components/SearchResults.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomePage />
-          </Route>
-          <Route path="/results">
-            <SearchResults />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <HomePage />
+            </Route>
+            <Route path="/results">
+              <SearchResults />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ChakraProvider>
   );
 }
 
--- web_frontend/src/Components/HomePage.js
+++ web_frontend/src/Components/HomePage.js
@@ -1,66 +1,45 @@
-import { useFormik, Formik } from "formik";
-import {
-  FormControl,
-  Input,
-  Form,
-  Field,
-  Center,
-  Heading,
-} from "@chakra-ui/react";
+import { Formik, Field, Form } from "formik";
+import { FormControl, Box, Image, Input, Center } from "@chakra-ui/react";
 import { useHistory } from "react-router-dom";
+import duckLogo from "./duckLogo.png";
 
 const HomePage = () => {
   let history = useHistory();
 
-  const formik = useFormik({
-    initialValues: {
-      search: "",
-    },
-    onSubmit: (values) => {
-      history.push("/results");
-      // TODO: Make API call to search firestore here then display results when I get them back
-      alert(values["search"]);
-    },
-  });
-
   return (
-    // <Formik
-    //   initialValues={{ name: "Sasuke" }}
-    //   onSubmit={(values) => {
-    //     history.push("/results");
-    //     alert(JSON.stringify(values, null, 2));
-    //   }}
-    // >
-    //   {(props) => (
-    //     <Form>
-    //       <Field name="name">
-    //         {({ field, form }) => (
-    //           <FormControl>
-    //             <Input {...field} id="name" placeholder="name" />
-    //           </FormControl>
-    //         )}
-    //       </Field>
-    //     </Form>
-    //   )}
-    // </Formik>
-
-    <div className="App">
-      <Center p={10} mt={350}>
-        <Heading as="h1">DuckDuckAbdaal</Heading>
-      </Center>
+    <>
       <Center>
-        {/* <Form onSubmit={formik.handleSubmit}> */}
-        <Input
-          height="50px"
-          w="500px"
-          type="text"
-          placeholder="Search any Ali Abdaal quote"
-          onChange={formik.handleChange}
-          value={formik.values.search}
-        />
-        {/* </Form> */}
+        <Box boxSize="md" mb={-10}>
+          <Image src={duckLogo} alt="DuckDuckAbdaal" />
+        </Box>
       </Center>
-    </div>
+      <Center mt={-55}>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={(values) => {
+            // TODO: Make API call to search firestore here then display results when I get them back
+            history.push("/results");
+          }}
+        >
+          {(props) => (
+            <Form>
+              <Field name="search">
+                {({ field, form }) => (
+                  <FormControl>
+                    <Input
+                      height="50px"
+                      w="700px"
+                      type="text"
+                      placeholder="Search any Ali Abdaal quote"
+                    />
+                  </FormControl>
+                )}
+              </Field>
+            </Form>
+          )}
+        </Formik>
+      </Center>
+    </>
   );
 };
 

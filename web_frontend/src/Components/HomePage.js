86b4c2a14a0edc2979d1fb8c4a1d7e0e7b724c4e
nutlope <he55@drexel.edu>
2021-05-03 00:29:28 -0400

    added footer

--- web_frontend/src/Components/HomePage.js
+++ web_frontend/src/Components/HomePage.js
@@ -11,50 +11,68 @@ const HomePage = ({ setSearchVal, searchVal }) => {
 
   return (
     <>
-      <Center>
-        <Box boxSize="lg" mb={-10} ml={19}>
-          <img src={duckLogo} alt="DuckDuckAbdaal" />
-        </Box>
-      </Center>
-      <Center mt={-55}>
-        <Search
-          width="700px"
-          height="50px"
-          setSearchVal={setSearchVal}
-          searchVal={searchVal}
-        />
-      </Center>
-      <div id="buttonContainer">
-        <Formik
-          initialValues={{ clicked: "" }}
-          onSubmit={() => {
-            let idx = Math.floor(Math.random() * luckySearches.length);
-            let val = luckySearches[idx];
-            setSearchVal({ search: val });
-            history.push({
-              pathname: "/results",
-              search: "?" + val,
-            });
-          }}
-        >
-          {() => (
-            <Form>
-              <Field name="search">
-                {({ field }) => (
-                  <Button
-                    {...field}
-                    colorScheme="gray"
-                    id="buttonItem"
-                    variant="outline"
-                    type="submit"
-                  >
-                    <span id="lucky">I'm Feeling Lucky</span>
-                  </Button>
-                )}
-              </Field>
-            </Form>
-          )}
-        </Formik>
+      <div id="wrapper">
+        <Center>
+          <Box boxSize="lg" mb={-10} ml={19}>
+            <img src={duckLogo} alt="DuckDuckAbdaal" />
+          </Box>
+        </Center>
+        <Center mt={-55}>
+          <Search
+            width="700px"
+            height="50px"
+            setSearchVal={setSearchVal}
+            searchVal={searchVal}
+          />
+        </Center>
+        <div id="buttonContainer">
+          <Formik
+            initialValues={{ clicked: "" }}
+            onSubmit={() => {
+              let idx = Math.floor(Math.random() * luckySearches.length);
+              let val = luckySearches[idx];
+              setSearchVal({ search: val });
+              history.push({
+                pathname: "/results",
+                search: "?" + val,
+              });
+            }}
+          >
+            {() => (
+              <Form>
+                <Field name="search">
+                  {({ field }) => (
+                    <Button
+                      {...field}
+                      colorScheme="gray"
+                      id="buttonItem"
+                      variant="outline"
+                      type="submit"
+                    >
+                      <span id="lucky">I'm Feeling Lucky</span>
+                    </Button>
+                  )}
+                </Field>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+      <div id="footer">
+        <div class="flex-item">
+          Made by{" "}
+          <b>
+            <a id="orange" href="https://twitter.com/Nutlope">
+              Hassan
+            </a>
+          </b>
+        </div>
+        <div class="flex-item">
+          GitHub
+          <a href="https://github.com/Nutlope/yt-search-engine">
+            <b id="orange"> Source Code</b>
+          </a>
+        </div>
       </div>
     </>
   );
--- web_frontend/src/Components/Results.js
+++ web_frontend/src/Components/Results.js
@@ -7,7 +7,7 @@ const Results = ({ video_url, title, text, quote }) => {
   return (
     <div className="Results">
       <Stack>
-        <Link color="teal.600" href={video_url} isExternal>
+        <Link color="#DE5833" href={video_url} isExternal>
           {title} <ExternalLinkIcon mx="2px" />
         </Link>
         <Text fontSize="md" as="b">
